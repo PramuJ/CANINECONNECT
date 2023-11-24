73aa3e5a7754f9a2616ae986cb59200fb8836fd1
Thaaruni <tharunidissanayake99@gmail.com>
2023-11-24 23:40:00 +0530

    First commit

---
+++ src/Forms/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegistrationForm from './DogRegistrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Helooooooooo</h1>
+      <DogRegistation/>
+    </div>
+  );
+}
+
+export default App;
